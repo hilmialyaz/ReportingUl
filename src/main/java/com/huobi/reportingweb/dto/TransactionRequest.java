fe03284e965980fba37632956d157a94e80072a7
kemal <kemal.alyaz@gmail.com>
2019-03-26 02:36:36 +0300

    GetTransaction service test with TDD

---
+++ src/main/java/com/huobi/reportingweb/dto/TransactionRequest.java
@@ -0,0 +1,13 @@
+package com.huobi.reportingweb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionRequest {
+    private String transactionId;
+
+}
